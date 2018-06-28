2fa408a0b167e718ef0f9d41bf800fcf3d99deca
YunYeoHyang <1016789542@qq.com>
2018-06-28 11:52:00 +0800

    JavaBean

--- hotel-pojo/src/main/java/com/czxy/hotel/domain/Stock.java
+++ hotel-pojo/src/main/java/com/czxy/hotel/domain/Stock.java
@@ -1,4 +1,16 @@
 package com.czxy.hotel.domain;
 
 public class Stock {
+
+    /*
+    *
+    * Stock_id		房源ID
+		Price		房价
+		Describe		描述
+		Stock_type	房源类型
+		Model_id		户型ID
+		Stock_ status	房源状态
+		Info_id		房屋信息ID
+		Configure_id	配置设施ID
+    * */
 }
